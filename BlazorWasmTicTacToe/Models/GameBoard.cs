ba8998138cc7cbf5ac3da7bc4f7ca037d3972928
Matthew Jones <10541872+exceptionnotfound@users.noreply.github.com>
2020-05-21 12:07:11 -0700

    Add project files.

---
+++ BlazorWasmTicTacToe/Models/GameBoard.cs
@@ -0,0 +1,28 @@
+﻿using BlazorWasmTicTacToe.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWasmTicTacToe.Models
+{
+    public class GameBoard
+    {
+        public GamePiece[,] Board { get; set; }
+
+
+        public GameBoard()
+        {
+            Board = new GamePiece[3, 3];
+
+            //Populate the Board with blank pieces
+            for (int i = 0; i <= 2; i++)
+            {
+                for (int j = 0; j <= 2; j++)
+                {
+                    Board[i, j] = new GamePiece(PieceStyle.Blank);
+                }
+            }
+        }
+    }
+}
---
+++ BlazorWasmTicTacToe/Models/GamePiece.cs
@@ -0,0 +1,23 @@
+﻿using BlazorWasmTicTacToe.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWasmTicTacToe.Models
+{
+    public class GamePiece
+    {
+        public PieceStyle Style;
+
+        public GamePiece()
+        {
+            Style = PieceStyle.Blank;
+        }
+
+        public GamePiece(PieceStyle color)
+        {
+            Style = color;
+        }
+    }
+}
